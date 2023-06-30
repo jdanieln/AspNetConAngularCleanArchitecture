ab9e1c5eec1ffe4dcf15ec6fcdd726ab54508727
Jose Narvaez <josedanielnarvaez452@gmail.com>
2023-06-30 11:00:21 -0600

    Initial Commit

---
+++ AspNetCoreWithAngularCleanArchitecture/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCoreWithAngularCleanArchitecture.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AspNetCoreWithAngularCleanArchitecture.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ AspNetCoreWithAngularCleanArchitecture/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreWithAngularCleanArchitecture.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
